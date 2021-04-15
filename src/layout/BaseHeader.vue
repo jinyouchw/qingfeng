7a29ee65eefcc4f985dc125c474999c7e272cd1a
huanhuanchen <2749499517@qq.com>
2021-04-15 09:15:30 +0800

    公用头部样式

--- src/layout/BaseHeader.vue
+++ src/layout/BaseHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="header">
-     <div>fasdfsda</div>
+  <div class="nav" flex ='dir:top'>
+     <div class="nav-content"></div>
      <router-view style="flex:1"></router-view>
   </div>
 </template>
@@ -10,7 +10,17 @@ import { defineComponent } from 'vue';
 
 
 export default defineComponent({
-  name: 'Header',
+  name: 'nav',
   components: { },
 });
 </script>
+<style scoped lang='scss'>
+$nav-height: 48px;
+.nav{
+  .nav-content{
+      height: $nav-height;
+      //box-shadow:0px 15px 10px -16px #999999;
+      box-shadow: 0 1px 8px 0 rgba(0, 0, 0, 0.08);
+  }
+}
+</style>
