358facd4035e98d13a5e3f6ad07dcd93d9cac4e4
huanhuanchen <2749499517@qq.com>
2021-04-16 17:42:26 +0800

    引入ant-design

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ import store from './store'
 import { registerComp } from './components/registerComp'
 
 import 'ant-design-vue/dist/antd.css';
-
+import Antd from 'ant-design-vue';
 import 'flex.css'
+import i18n from './language/index'
 
 const app =  createApp(App)
 
@@ -15,4 +16,6 @@ registerComp(app) //注册全局组件
 
 app.use(store)
   .use(router)
+  .use(Antd)
+  .use(i18n)
   .mount("#app");
